9ed2e8b9e9efeb63eaf3f917d73da866ab4afc59
kabuto64425 <kabuto64425_higasumi52@yahoo.co.jp>
2025-06-11 17:50:00 +0900

    Update virtualStickInput.ts

--- src/game/virtualStickInput.ts
+++ src/game/virtualStickInput.ts
@@ -11,12 +11,14 @@ export class VirtualStickInput {
     // VirtualJoyStick は Scene に強く結びついているため、
     // SceneContext を通さず直接 scene を渡すのが正しい設計判断らしい。 by chatgpt
     static setup(scene: Phaser.Scene) {
+        // 最大半径は89
+        // できればbaseを最大半径の○に×の十字キーっぽい見た目にしたい
         const joystick = (scene.plugins.get('rexVirtualJoyStick') as VirtualJoyStickPlugin).add(scene, {
             x: 89,
             y: 400,
             radius: 89,
             base: scene.add.circle(0, 0, 89, 0x888888),
-            thumb: scene.add.circle(0, 0, 70, 0xcccccc),
+            thumb: scene.add.circle(0, 0, 80, 0xcccccc),
             // 方向の制限（水平・垂直）も可能
             dir: '4dir',
             forceMin: 0,
